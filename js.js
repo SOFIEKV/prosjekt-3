9f5d7e7e20a8ebf297e82020a382641c234c1884
SOFIEKV <sofie@kvadsheim.priv.no>
2024-05-20 01:43:40 +0200

    ghj

--- js.js
+++ js.js
@@ -2,15 +2,13 @@ let paa = "paa"
 let px = 700
 
 window.onscroll = function () {
-
     if (window.scrollY >= px || window.pageYOffset >= px) {
-        document.getElementsByTagName("header")[0].classList.add(paa)
-
-
+        document.getElementsByClassName("header")[0].classList.add(paa)
     } else {
-        document.getElementsByTagName("header")[0].classList.remove(paa);
+        document.getElementsByClassName("header")[0].classList.remove(paa);
     }
 }
+
 
 const kildeKnapp = document.getElementById("kildeKnapp")
 const kilder = document.querySelector(".kilder")
