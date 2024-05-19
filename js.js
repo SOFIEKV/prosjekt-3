5aca6adea249c69e9c76622fbc2519afec4b3173
SOFIEKV <sofie@kvadsheim.priv.no>
2024-05-20 00:05:34 +0200

    vgh

---
+++ js.js
@@ -0,0 +1,26 @@
+let paa = "paa"
+let px = 700
+
+window.onscroll = function () {
+
+    if (window.scrollY >= px || window.pageYOffset >= px) {
+        document.getElementsByTagName("header")[0].classList.add(paa)
+
+
+    } else {
+        document.getElementsByTagName("header")[0].classList.remove(paa);
+    }
+}
+
+const kildeKnapp = document.getElementById("kildeKnapp")
+const kilder = document.querySelector(".kilder")
+
+kildeKnapp.addEventListener("click", function () {
+    if (kilder.style.display == "none") {
+        kilder.style.display = "block"
+    }
+    else {
+        kilder.style.display = "none";
+    }
+})
+
