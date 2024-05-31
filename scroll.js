f219a88468b704e84ec6485df82e16e63513ac3b
SOFIEKV <sofie@kvadsheim.priv.no>
2024-05-31 20:28:42 +0200

    dfgh

--- scroll.js
+++ scroll.js
@@ -1,4 +1,25 @@
 
+let paa = "paa";
+let px = 600;
+let maxPx = 700;
+
+window.onscroll = function () {
+    document.querySelectorAll(".KF2").forEach((section, index) => {
+        let sectionTop = section.offsetTop;
+        let sectionBottom = sectionTop + section.offsetHeight;
+
+        if (window.scrollY + window.innerHeight >= sectionTop + px) {
+            section.classList.add(paa);
+        } 
+
+        if (window.scrollY + window.innerHeight >= sectionBottom + maxPx) {
+            section.classList.remove(paa);
+        }
+    });
+};
+
+
+/*
 let paa = "paa"
 let px = 600
 let maxPx = 700
@@ -8,10 +29,11 @@ window.onscroll = function () {
         document.getElementsByClassName("KF2")[0].classList.add(paa)
     } /*else {
         document.getElementsByClassName("KF2")[0].classList.remove(paa);
-    }*/
+    }
     if(window.scrollY >= maxPx || window.pageYOffset >= maxPx){
         document.getElementsByClassName("KF2")[0].classList.remove("paa")
     }
     
 }
 
+*/
